6c9d8b832eddd8a0b684010ff16b709590db1b2b
Deeksha1702 <116424631+Deeksha1702@users.noreply.github.com>
2024-10-13 18:55:42 +0530

    Updated main.py to add sentiment analyzer

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fetcher import fetch_article
 from parser import parse_article
 from display import display_article
+from sentiment_analyzer import analyze_sentiment  # Import the sentiment analyzer
 
 urls = [
     "https://timesofindia.indiatimes.com/city/bengaluru/discover-karnatakas-hidden-gems-8-must-visit-places-from-hampi-to-badami/photostory/111403217.cms",
     "https://timesofindia.indiatimes.com/city/bengaluru/renukaswamy-neha-hiremath-anjali-ambigera-5-most-gruesome-murders-in-karnataka-that-shook-entire-nation-in-recent-past/photostory/111110939.cms",
-    # Add more URLs here if needed
 ]
+
 for url in urls:
     content = fetch_article(url)
     article_data = parse_article(content)
-    display_article(article_data)
+    article_sentiment = analyze_sentiment(article_data['content'])
+
+    display_article(article_data, article_sentiment)
