df703bdfa2f3008db37345d659603b09f56f1b50
Deeksha1702 <116424631+Deeksha1702@users.noreply.github.com>
2024-10-13 18:58:57 +0530

    Update display.py to display sentiment

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
-def display_article(article_data):
+def display_article(article_data, sentiment):
     print("Title:", article_data["headline"])
-    print("Date & Time:", article_data["date_and_time"])
+    print("Date & Time:", article_data["date_and_time"])  # Display the date and time
     print("Content:\n", article_data["content"])
-    print("\n" + "="*50 + "\n")
+    print("Sentiment:", sentiment)
+    print("\n" + "=" * 50 + "\n")
